be697487875adfbb251550924d9d2042803aa7ac
BA-HADDOU Ayoub <106288338+bahaddouayoub@users.noreply.github.com>
2023-06-17 11:05:43 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12"
   backend "s3" {
-    bucket = "peaqock-state.terraform"
+    bucket = "ayoubsatet"
     key    = "state.tfstate"
     region = "us-east-1"
   }
